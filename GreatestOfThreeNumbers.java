52eeda1da7ac497ecfd60d4316c76b14c727c6b1
Harshithapacha <123634151+Harshithapacha@users.noreply.github.com>
2024-02-29 23:28:22 +0530

    Add files via upload

---
+++ GreatestOfThreeNumbers.java
@@ -0,0 +1,28 @@
+public class GreatestOfThreeNumbers 
+{
+
+public static void main(String[] args)
+
+{
+
+int a=20;
+
+int b=30;
+
+int c=15;
+
+if(a>b && a>c)
+
+System.out.println("a is greater");
+
+else if(b>c)
+
+System.out.println("b is greater");
+
+else 
+
+System.out.println("c is greater");
+
+}
+
+}
